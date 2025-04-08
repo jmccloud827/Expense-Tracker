7fec9b2155439b4be7139dd56523eed9ee8fdb09
Jacob McCloud <jmccloud@uwm.com>
2025-04-08 11:31:22 -0400

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Jacob McCloud on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
